453d3e9f701f129bba646df63129889acfa0e491
sgiang1610 <songiang1610@gmail.com>
2025-02-17 14:27:36 +0700

    delete note files

--- src/script.js
+++
@@ -1,8 +0,0 @@
-const { chromium } = require('playwright');
-
-(async () => {
-    const args = process.argv.slice(2);
-    const checkedBoxes = args.length > 0 ? JSON.parse(args[0]) : {};
-
-    console.log('Checked values:', checkedBoxes);
-})();
